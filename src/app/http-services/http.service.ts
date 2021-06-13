9d61fae26eef7bb4717c4a7ca1e04168b5836642
Kernel Panic <jgg.jobs.development@gmail.com>
2021-06-13 20:16:48 +0200

    Http services

--- src/app/http-services/http.service.ts
+++ src/app/http-services/http.service.ts
@@ -1,11 +1,19 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export abstract class HttpService {
+
+  protected uri: string;
+
   protected constructor(
-    protected httpClient: HttpClient
+    protected httpClient: HttpClient,
+    @Inject('applicationServerHost') protected readonly applicationServerHost: string
   ) {}
+
+  protected getUrl(): string {
+    return `${this.applicationServerHost}/${this.uri}`;
+  }
 }
