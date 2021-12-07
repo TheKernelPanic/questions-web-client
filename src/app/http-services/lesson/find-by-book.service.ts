0b58b443daf9f8f76374291eff47e4528681b447
KernelPanic <kernelpanic0x000007C4@protonmail.com>
2021-12-07 20:26:48 +0100

    Fix.

--- src/app/http-services/lesson/find-by-book.service.ts
+++ src/app/http-services/lesson/find-by-book.service.ts
@@ -1,14 +1,14 @@
 import {HttpService} from "../http.service";
 import {Observable} from "rxjs";
-import {Book} from "../../domain/interfaces";
+import {Book, Lesson} from "../../domain/interfaces";
 import {Injectable} from "@angular/core";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FindByBookService extends HttpService {
-  public request(bookId: string): Observable<Book[]> {
-    return this.httpClient.get(
+  public request(bookId: string): Observable<Lesson[]> {
+    return this.httpClient.get<Lesson[]>(
       this.applicationServerHost + '/getLessonsByBook/' + bookId
     );
   }
