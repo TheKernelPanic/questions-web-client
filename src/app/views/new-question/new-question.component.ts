515c84c0e604cfa6096502fd9a1f7f259c68b332
DESKTOP-CFFKDUN\jorge <kernelpanic0x000007C4@protonmail.com>
2022-01-23 16:24:54 +0100

    Refactor.

--- src/app/views/new-question/new-question.component.ts
+++ src/app/views/new-question/new-question.component.ts
@@ -1,13 +1,8 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Book, Help, Lesson, Question, Topic} from "../../domain/interfaces";
 import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {FindAllService} from "../../http-services/topic/find-all.service";
 import {HttpErrorResponse} from "@angular/common/http";
-import {FindByTopicService} from "../../http-services/book/find-by-topic.service";
-import {FindByBookService} from "../../http-services/lesson/find-by-book.service";
 import {Title} from "@angular/platform-browser";
-import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
-import {NewLessonService} from "../../http-services/lesson/new-lesson.service";
 import {NewQuestionService} from "../../http-services/question/new-question.service";
 import {Router} from "@angular/router";
 
@@ -18,104 +13,30 @@ import {Router} from "@angular/router";
 })
 export class NewQuestionComponent implements OnInit {
 
-  @ViewChild('newLessonModal') private newLessonModal: ElementRef<NgbModalRef>;
-
   public form: FormGroup;
   public maxAmountAnswers: number = 6;
   public loader: boolean = false;
-  public topics: Topic[];
-  public books: Book[];
-  public lessons: Lesson[];
   public helps: Help[];
   public formAsErrors: boolean;
   public lessonSelected: Lesson|null;
   public topicSelected: Topic|null;
-  public bookSelected: Book|null;
-  public newLessonForm: FormGroup;
 
   public constructor(
     private formBuilder: FormBuilder,
-    private topicService: FindAllService,
-    private bookService: FindByTopicService,
-    private lessonService: FindByBookService,
     private title: Title,
-    private modalService: NgbModal,
-    private newLessonService: NewLessonService,
     private newQuestionService: NewQuestionService,
     private router: Router
   ) {
-    this.loader = true;
-    this.topics = [];
-    this.books = [];
-    this.lessons = [];
+    this.loader = false;
     this.helps = [];
     this.formAsErrors = false;
     this.lessonSelected = null;
-    this.bookSelected = null;
     this.topicSelected = null;
-  }
-
-  public topicsHttpRequest(): void {
-    this.loader = true;
-    this.topicService.request().subscribe({
-      next: (response: Topic[]) => {
-        if (response.length) {
-          this.topics = response;
-          this.topicSelected = this.topics[0];
-          this.bookHttpService();
-        }
-      },
-      error: (error: HttpErrorResponse) => {
-        throw new Error(error.message);
-      },
-      complete: () => {
-        this.loader = false;
-      }
-    })
-  }
-
-  public bookHttpService(): void {
-    this.loader = true;
-    this.bookService.request(this.topicSelected?.id as string).subscribe({
-      next: (response: Book[]) => {
-        if (response.length) {
-          this.books = response;
-          this.bookSelected = this.books[0];
-          this.lessonHttpService();
-        }
-      },
-      error: (error: HttpErrorResponse) => {
-        throw new Error(error.message);
-      },
-      complete: () => {
-        this.loader = false;
-      }
-    })
-  }
-
-  public lessonHttpService(): void {
-    this.loader = true;
-    this.lessonService.request(this.bookSelected?.id as string).subscribe({
-      next: (response: Lesson[]) => {
-        if (response.length) {
-          this.lessons = response;
-        }
-      },
-      error: (error: HttpErrorResponse) => {
-        throw new Error(error.message);
-      },
-      complete: () => {
-        this.loader = false;
-      }
-    })
   }
 
   public ngOnInit(): void {
     this.title.setTitle('Backoffice - Question creation');
-
-    this.topicsHttpRequest();
     this.initializeForm();
-
     this.addNewAnswer();
     this.addNewAnswer();
     this.addNewAnswer();
@@ -188,35 +109,6 @@ export class NewQuestionComponent implements OnInit {
     question.helps = this.helps
 
     return question;
-  }
-
-  public saveNewLesson(): void {
-    if (!this.newLessonForm.valid) {
-      return;
-    }
-    const lesson: Lesson = {
-      description: (this.newLessonForm.get('description') as FormControl).value,
-      position: this.lessons.length+1,
-      book: this.bookSelected
-    }
-    this.newLessonService.request(lesson).subscribe({
-      next: () => {
-        this.modalService.dismissAll();
-        this.lessonHttpService();
-      },
-      error: (error: HttpErrorResponse) => {
-        throw new Error(error.message);
-      }
-    });
-  }
-
-  public openNewLessonModal(): void {
-    this.newLessonForm = new FormGroup({
-      description: new FormControl('', Validators.required)
-    });
-    this.modalService.open(this.newLessonModal, {
-      centered: true
-    });
   }
 
   public addHelp(help: Help): void {
